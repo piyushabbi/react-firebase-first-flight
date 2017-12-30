e98a57a97ca995ddd135b7d7e349677c55c42cc7
Piyush Abbi <pabbi@sapient.com>
2017-12-30 18:38:37 +0530

    Set key fb obj

--- src/App.js
+++ src/App.js
@@ -6,15 +6,29 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: null
+      data: null,
+      newData: ''
     };
+    // Event Bindings
+    this.submitHandler = this.submitHandler.bind(this);
+    this.changeHandler = this.changeHandler.bind(this)
   }
   componentDidMount() {
-    database.ref('/').on('value', (snapshot) => {
+    database.ref().on('value', (snapshot) => {
       this.setState({
         data: snapshot.val()
       })
     });
+  }
+  submitHandler(e) {
+    e.preventDefault();
+    database.ref().child('AMAZINGDATA').set(this.state.newData);
+  }
+  changeHandler(e) {
+    const newData = e.target.value;
+    this.setState({
+      newData
+    })
   }
 
   render() {
@@ -24,9 +38,16 @@ class App extends Component {
           <h2>Welcome to React and Firebase</h2>
         </div>
         <pre className="App--data">
-          One day, some data from Firebase will go here.
-          <h2>{ this.state.data == null ? 'Loading...' : this.state.data }</h2>
+          One day, some data from Firebase will go here.<br />
+          { JSON.stringify(this.state.data, null, 2) }
         </pre>
+        <form className="App-form" onSubmit={this.submitHandler}>
+          <input 
+            type="text" 
+            onChange={this.changeHandler} 
+            value={this.state.newData} />
+          <input type="submit" />
+        </form>
       </div>
     );
   }
